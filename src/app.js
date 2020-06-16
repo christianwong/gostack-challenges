f0a23772d1e5547b6703f3c060efd2024b9b4a63
Christian Wong <dev@christianwong.com>
2020-06-16 19:55:19 -0300

    adding rest logic

--- src/app.js
+++ src/app.js
@@ -1,33 +1,92 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express")
+const cors = require("cors")
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4")
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const repositories = [];
+const repositories = []
 
 app.get("/repositories", (request, response) => {
-  // TODO
-});
+  return response.json(repositories)
+})
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body
+
+  const repo = {
+    id: uuid(), 
+    title, 
+    url, 
+    techs, 
+    likes: 0
+  }
+
+  repositories.push(repo)
+
+  return response.json(repo)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body
+  const {id} = request.params
+  
+  const repoIdx = repositories.findIndex(repo => repo.id === id)
+  if (repoIdx < 0) {
+    return response.status(400).json({error: "Invalid Repository Id."})
+  }
+
+  const repo = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repoIdx].likes
+  }
+
+  repositories[repoIdx] = repo
+
+  return response.status(204).send()
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params
+  
+  const repoIdx = repositories.findIndex(repo => repo.id === id)
+  if (repoIdx < 0) {
+    return response.status(400).json({error: "Invalid Repository Id."})
+  }
+
+  repositories.splice(repoIdx, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params
+  
+  const repoIdx = repositories.findIndex(repo => repo.id === id)
+  if (repoIdx < 0) {
+    return response.status(400).json({error: "Invalid Repository Id."})
+  }
+
+  const {title, url, techs, likes} = repositories[repoIdx]
+  const newLikes = likes+1
+
+  const repo = {
+    id,
+    title,
+    url,
+    techs,
+    likes: newLikes
+  }
+
+  repositories[repoIdx] = repo
+
+  return response.status(204).send()
 });
 
-module.exports = app;
+module.exports = app
