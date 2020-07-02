028e105a48e49234697a8b92ef7e8b683101aa5e
Christian Wong <dev@christianwong.com>
2020-07-02 19:07:47 -0300

    :sparcles: adding validation for type

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,10 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: RequestDTO): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid');
+    }
+
     const { total } = this.transactionsRepository.getBalance();
     if (type === 'outcome' && value > total) {
       throw Error("you don't have all that money!");
