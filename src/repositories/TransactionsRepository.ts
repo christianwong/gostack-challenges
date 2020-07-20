3f77bcb6f817ec627115900a4be991b55947535d
Christian Wong <dev@christianwong.com>
2020-07-20 20:42:44 -0300

    should be able to list transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,24 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions
+      .map(transaction =>
+        transaction.type === 'income'
+          ? { income: Number(transaction.value) }
+          : { outcome: Number(transaction.value) },
+      )
+      .reduce(
+        (acc, cur) => ({
+          income: acc.income + (cur.income || 0),
+          outcome: acc.outcome + (cur.outcome || 0),
+        }),
+        { income: 0, outcome: 0 },
+      );
+
+    const balance = { income, outcome, total: income - outcome } as Balance;
+    return balance;
   }
 }
 
